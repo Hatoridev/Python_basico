bbc1258956c06a3db0b9cbba2a5ed5204c456eaf
Hatoridev <jonasdev21@outlook.com>
2024-04-22 13:28:08 -0300

    Adicionando arquivos da aula 02 de Python Básico

---
+++ aula02/Script.py
@@ -0,0 +1,28 @@
+# Início do diálogo
+
+print("Geovana atende")
+input("Pressione Enter para continuar...")
+
+print("Ana diz: Geh, você gosta de filmes de terror?")
+input("Pressione Enter para continuar...")
+
+print("Geovana: Gosto sim!")
+input("Pressione Enter para continuar...")
+
+print("Ana: Já assistiu Pânico 6?")
+input("Pressione Enter para continuar...")
+
+input("Geovana:Não, amiga. ")
+input("Pressione Enter para continuar...")
+
+print("Ana: Vamos assistir?")
+input("Pressione Enter para continuar...")
+
+print("Geovana: Claro, amiga!")
+
+# Comentários explicativos:
+
+# As linhas de código acima simulam um diálogo entre Geovana e Ana.
+
+# Cada input() pausa a execução até que o usuário pressione Enter para continuar.
+
---
+++ aula02/World.py
@@ -0,0 +1,9 @@
+def print_hello_world():
+    print("Hello World!")
+
+# Chamando a função para imprimir "Hello World"
+print_hello_world()
+
+# Explicação:
+
+# A linha acima chama a função print_hello_world(), que imprime "Hello World" no console.
